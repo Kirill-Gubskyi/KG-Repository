6b1f2a2fb7b32d4e82582de07bcab95410305a49
Kirill <Kirill_Gubskii_2@mail.ru>
2016-06-21 13:10:24 +0200

    index.php change 2

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
 $menu = [
     ['link'=>'/', 'title'=>'Home'],
-    ['link'=>'/services', 'title'=>'Services', 'children'=>[
-         ['link'=>'/serv1', 'title'=>'Serv 1'],
-         ['link'=>'/serv2', 'title'=>'Serv 2'],
-         ['link'=>'/serv3', 'title'=>'Serv 3'],
-    ]],
+    ['link'=>'/services', 'title'=>'Services'],
     ['link'=>'/news', 'title'=>'News'],
     ['link'=>'/contacts', 'title'=>'Write to us']
+    ['link'=>'/help', 'titile'=>'Help']
 ];
 function showMenu($menuArray,$n=10)
 {
