14c63e465338306dd42fde914c6f96cb1695011c
Kirill <Kirill_Gubskii_2@mail.ru>
2016-06-25 14:15:51 +0200

    Home work SQL №1

--- TestPhp.php
+++ TestPhp.php
@@ -1,10 +1,24 @@
 <?php
-$Kir=array();
-$Kir=['One'=>1,'Two'=>2,'Three'=>3];
-$a=$Kir['One'];
-$b=$Kir ['Two'];
-$c=$Kir ['Three'];
-echo "$a <br> $b  $c";
+$menu = [
+    ['link'=>'/', 'title'=>'Home'],
+    ['link'=>'/services', 'title'=>'Services', 'children'=>[
+         ['link'=>'/serv1', 'title'=>'Serv 1'],
+         ['link'=>'/serv2', 'title'=>'Serv 2'],
+         ['link'=>'/serv3', 'title'=>'Serv 3'],
+    ]],
+    ['link'=>'/news', 'title'=>'News'],
+    ['link'=>'/contacts', 'title'=>'Write to us']
+];
+function showMenu($menuArray)
+{
+    echo '<ul>';
+    foreach($menuArray AS $keyl=>$item)
+    {
+     echo '<li><a href="'.$item['link'].'">'.$item['title'].'</a>
+           </li>';
+    }
+    echo '</ul>';
+}
 
-
-
+echo showMenu($menu);
+?>
